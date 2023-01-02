750535bea6b15423dd19a0abede2ddd17ab48af2
Nicolas <nicolasgabriel.developer@gmail.com>
2023-01-02 16:58:18 -0300

    Criando os dois componentes principais
    Criando os dois componentes principais da tela, o BarraLateral e o Formulario

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <main class="columns is-gapless is-multiline">
+    
+    <div class="column is-one-quarter">
+      <BarraLateral/>
+    </div><!--coluna 1-->
+   
+    <div class="column is-three-quarter">
+      <Formulario/> 
+    </div><!--coluna 2-->
+  
+  </main><!--corpo principal-->
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import BarraLateral from '../src/components/BarraLateral.vue'
+import Formulario from '../src/components/Formulario.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    BarraLateral,
+    Formulario
   }
 });
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,35 @@
+<!--Aqui vai o HTML-->
+<template>
+    <header>
+        <h1>
+            <img src="../assets/logo.png" alt="logo">
+        </h1>
+    </header>
+</template>
+
+<!--Aqui vai o TypeScript-->
+<script lang="ts">
+
+import { defineComponent } from 'vue';
+
+export default defineComponent ({
+    name: "BarraLateral"
+} )
+
+</script>
+
+<!--Aqui vai o Css (o SCOPED é importante para o style funcionar apenas nesse componente)-->
+<style scoped>
+header{
+    background-color: #0d3b66;
+    width: 100%;
+    height: 100vh;
+    padding: 1rem;
+}
+@media only screen and (max-width: 768px){
+    header{
+        padding: 2.5rem;
+        height: auto;
+    }
+}
+</style>
