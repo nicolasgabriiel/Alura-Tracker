73d5b29ddb978b65fdbe62677cd62f10d4707070
Nicolas <nicolasgabriel.developer@gmail.com>
2023-01-03 15:22:36 -0300

    adicionando toda a lógica e implementação do contador

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -8,15 +8,15 @@
                 <div class="is-flex is-align-items-center is-justify-content-space-between">
 
                     <section>
-                        <strong>00:00:00</strong>
+                        <strong>{{ tempoDecorrido }}</strong>
                     </section>
-                    <button class="button">
+                    <button class="button" @click="iniciar">
                         <span class="icon">
                             <i class="fas fa-play"></i>
                         </span>
                         <span>play</span>
                     </button>
-                    <button class="button">
+                    <button class="button" @click="finalizar">
                         <span class="icon">
                             <i class="fas fa-stop"></i>
                         </span>
@@ -33,7 +33,30 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-    name: "formulario-completo"
+    name: "formulario-completo",
+    data (){
+        return{
+            tempoEmSegundos: 0,
+            cronometro: 0
+        }
+    },
+    computed:{
+        tempoDecorrido(): string{
+            return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11,8)
+        }
+    },
+    methods:{
+        iniciar (){
+            console.log('iniciou')
+            this.cronometro = setInterval( () => {
+                this.tempoEmSegundos++
+            }, 1000)
+        },
+        finalizar (){
+            clearInterval(this.cronometro)
+        },
+    }
+
 })
 
 </script>
