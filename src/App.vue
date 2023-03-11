2e1828c9e270b16f8b11f97636aae26da668a71d
Nicolas <nicolasgabriel.developer@gmail.com>
2023-03-11 15:36:47 -0300

    Zerando a pasta pra começas projeto novamente

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,21 @@
 <template>
-  <main class="columns is-gapless is-multiline">
-    
-    <div class="column is-one-quarter">
-      <BarraLateral/>
-    </div><!--coluna 1-->
-   
-    <div class="column is-three-quarter">
-      <Formulario @aoSalvarTarefa="salvarTarefa"/> 
-      <div class="lista">
-            <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa"/>
-        </div>
-    </div><!--coluna 2-->
   
-  </main><!--corpo principal-->
+    
+  <h1>Zerando o pasta</h1>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import BarraLateral from '../src/components/BarraLateral.vue'
-import Formulario from '../src/components/Formulario.vue'
-import  Tarefa  from './components/Tarefa.vue'
-import ITarefa from './interfaces/ITarefa';
+
 
 export default defineComponent({
   name: 'App',
-  emits:['aoSalvarTarefa'] ,
-  components: {
-    BarraLateral,
-    Formulario,
-    Tarefa,
   },
-  data(){
-        return{
-            tarefas: [] as ITarefa[]
-        }
-    },
-    methods: {
-      salvarTarefa(tarefa: ITarefa){
-        this.tarefas.push(tarefa)
-      }
-    }
-});
+
+)
 </script>
 
 
 <style scoped>
-.lista{
-  padding: 1.25rem;
-}
 
 </style>
