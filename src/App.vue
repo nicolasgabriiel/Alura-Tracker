c94ad9eb3d34d5db0f817ed2d30ffd9871d76ecf
Nicolas <nicolasgabriel.developer@gmail.com>
2023-03-14 19:04:28 -0300

    FEAT: Barra Lateral
    Adicionando e criando a Barra Lateral dentro do nosso app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <template>
-  
-    
-  <h1>Zerando o pasta</h1>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <BarraLateral/>
+    </div>
+    <div class="column is-three-quarter">
+      
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import BarraLateral from './components/BarraLateral.vue'
 
 
 export default defineComponent({
   name: 'App',
-  },
-
-)
+  components:{
+    BarraLateral,
+  }
+})
 </script>
 
 
---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+    <h1>
+        <img src="../assets/logo.png">
+    </h1>
+  </header>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name:'Barra-Lateral'
+})
+</script>
+
+<style scoped>
+@media only screen and (max-width: 768px){
+    header{
+        padding: 2.5rem;
+        height: auto;
+    }
+}
+</style>
